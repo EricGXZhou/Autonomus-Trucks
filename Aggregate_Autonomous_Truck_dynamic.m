6f92feec859445bd70b6f0f606331e50c4e011bc
Kerem Cosar <62224903+kcosar@users.noreply.github.com>
2025-10-09 13:04:59 -0400

    played with values, no change

--- Aggregate_Autonomous_Truck_dynamic.m
+++ Aggregate_Autonomous_Truck_dynamic.m
@@ -6,7 +6,7 @@ clc
 
 %% Parameters
 D = 0.9; % disutility
-Ay = 7; % productivity of good Y
+Ay = 3; % productivity of good Y
 As = 1; % productivity of short haul
 Al = 1; % productivity of long haul
 alpha = 0.5; % the share of Y
